1265c42d78d2490735255c9009034351c6051d52
Mats <131871301+matsinator123@users.noreply.github.com>
2025-11-05 12:01:53 +0100

    Commit 3

--- instance_testing.py
+++ instance_testing.py
@@ -2,3 +2,12 @@ from Input_validation_task import UserInputValidator
 
 instance1 = UserInputValidator()
 instance2 = UserInputValidator()
+
+list1 = ["10", "-5", "abc", "42", "0"]
+list2 = ["-1", "xyz", "100", "200", "300"]
+
+valid_integers1 = instance1.validate_positive_integer(list1)
+valid_integers2 = instance2.validate_positive_integer(list2)
+
+print(instance1.validation_message())
+print(instance2.validation_message())
